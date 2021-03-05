f7e5d5b48b66a664caa297f86f93a5f82b3c9a66
noisyneuron <noisyneuron@users.noreply.github.com>
2021-03-05 17:44:56 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+const obj2gltf = require('obj2gltf')
+const fs = require('fs')
+const path = require('path')
+const yargs = require('yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+
+
+
+function convert(inputDir, outputDir, file) {
+  const input = path.resolve(inputDir, file)
+  const output = path.resolve(outputDir, file)
+  console.log(`converting ${input}`)
+  return obj2gltf(input, { separate: true, outputDirectory: outputDir })
+    .then(function (gltf) {
+      const data = Buffer.from(JSON.stringify(gltf));
+      fs.writeFileSync(output, data);
+    })
+    .catch(error => console.log(error))
+}
+
+async function main() {
+  if (argv.input && argv.output) {
+    const inputDir = path.resolve(__dirname, argv.input)
+    const outputDir = path.resolve(__dirname, argv.output)
+    const files = fs.readdirSync(inputDir).filter(e => e.match(/.*\.(obj)$/ig))
+
+    if (files.length === 0) {
+      console.log(`no .obj files in ${inputDir} ... BYE`)
+      process.exit(1)
+    }
+
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+
+    for (const file of files) {
+      await convert(inputDir, outputDir, file)
+    }
+
+  } else {
+    console.log('gimme input and output directories yo')
+  }
+}
+
+main()
